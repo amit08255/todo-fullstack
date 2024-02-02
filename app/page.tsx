1e6fa1083a24133abf311627e294b02e955d509f
amit08255 <amitcute3@gmail.com>
2024-02-02 08:17:52 +0530

    feat(tasks): Added tasks table

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,18 @@
+'use client';
+
+import {ListView} from "../components/table/ListView";
+import {useEffect, useState} from "react";
 
 export default function Page() {
-    return <div>Hello</div>
+    const [isDomLoaded, setIsDomLoaded] = useState(false);
+
+    useEffect(() => {
+        setIsDomLoaded(true);
+    }, []);
+
+    if (!isDomLoaded) {
+        return null;
+    }
+
+    return <ListView />
 }
